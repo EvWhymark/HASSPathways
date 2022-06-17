a87285315aa5a938e591fdc39b2039e85222bb7c
Michael Anderson <michael.j.anderson@me.com>
2022-06-17 12:31:17 -0400

    Login guard for frontend

---
+++ frontend/src/components/guard.vue
@@ -0,0 +1,18 @@
+import axios from 'axios'
+export const guard = (to, from, next) => {
+
+    const endpoint = 'http://127.0.0.1:5000/guard'
+    axios.get(endpoint)
+        .then(response => {
+            if(response.auth == 1) {
+                return next()
+            } else {
+                const logpoint = 'http://127.0.0.1:5000/login'
+                axios.post(logpoint, {
+                    nxt:next,
+                    ticket:"",
+                })
+            }
+        })
+
+};
