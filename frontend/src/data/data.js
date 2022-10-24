f5424bb95eccf37549994e0d8ac90940f9aaa204
Jichuan14 <91489333+Jichuan14@users.noreply.github.com>
2022-10-24 15:26:01 -0400

    update course main page

--- frontend/src/data/data.js
+++ frontend/src/data/data.js
@@ -1,13 +1,18 @@
 import pathwayCategoriesJSON from './json/pathway_categories.json'
+import minor_categories_json from './json/minor_categories.json'
 import yearsJSON from './json/years.json'
 
 // Neatify JSON data:
 // Sort pathways in pathway categories
 pathwayCategoriesJSON.forEach(category => category.pathways.sort());
+//Sort minor categories
+minor_categories_json.forEach(minor=>minor.minor.sort())
 
 // Prevent accidental modification
 Object.freeze(yearsJSON);
 Object.freeze(pathwayCategoriesJSON);
+Object.freeze(minor_categories_json);
 
 export const pathwayCategories = pathwayCategoriesJSON;
 export const years = yearsJSON;
+export const minor_categories=minor_categories_json;
