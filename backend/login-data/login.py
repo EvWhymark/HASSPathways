d1f764d311484482943ce04c2bd3aa080b151787
nicdicosimo <95448450+dicosn@users.noreply.github.com>
2022-11-13 14:19:08 -0500

    Wow an improvement of sorts
    wow

--- backend/login-data/login.py
+++ backend/login-data/login.py
@@ -2,6 +2,12 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from passlib.context import CryptContext
+# Import smtplib for the actual sending function
+import smtplib
+
+# Import the email modules we'll need
+from email.message import EmailMessage
+
 
 db = SQLAlchemy()
 app = Flask(__name__)
@@ -18,7 +24,7 @@ class Entry(db.Model):
     email = db.Column(db.String(1024), unique=True)
     password = db.Column(db.String(1024))
 
-# Entry(id=1, first='Nick', last='Dicosimo', email='asbasf@gasd.com', password='sadfsadf')
+# Entry(id=1, first='Big', last='Dinkus', email='asbasf@gasd.com', password='sadfsadf')
 
 def hash(password):
     return CryptContext(schemes=["bcrypt"], deprecated="auto").hash(password)
@@ -62,7 +68,7 @@ def login():
 #        email_got = 
 
 #maybe reset password?
-@app.route('/reset_password', methods = ["GET", "POST"]) #what is this one for? Post/Get? Post = send data to fnction, Get = give data to client
+@app.route('/change_password', methods = ["GET", "POST"]) #what is this one for? Post/Get? Post = send data to fnction, Get = give data to client
 def reset_password():
     if request.method == 'POST':
         email_got = request.args["email"]
@@ -77,6 +83,15 @@ def reset_password():
     #how to sennd passwod
     #generate unique number, store it somewhere(local) json file: key: name, code check if code same
 
+@app.route('/reset_password', methods = ["GET", "POST"]) #what is this one for? Post/Get? Post = send data to fnction, Get = give data to client
+def reset_password():
+    if request.method == 'POST':
+        email_got == request.args["email"]
+
+           #rand is good
+
+    #how to sennd passwod
+    #generate unique number, store it somewhere(local) json file: key: name, code check if code same
 
 #change personal info?
 @app.route('/change_info', methods = ["GET", "POST"])
